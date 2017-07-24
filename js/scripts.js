1e614178b6d46cbedcec3812c44fcbe79fe97368
Bart Webber, Kheifer Fuller and Paul Guevarra <bart@>
2017-07-24 14:14:01 -0700

    add index search for loop to return type of food to output

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,31 @@ function Foodfinder(type, location, truck) {
   this.type = type;
   this.location = location;
   this.truck = truck;
-  this.menuItem = menuItem;
-  this.price = price;
-  this.description = [];
+  // this.menuItem = menuItem;
+  // this.price = price;
+  // this.description = [];
 }
 
 var grilledCheeseGrill = new Foodfinder("American", "SW 10th & Alder", "Grilled Cheese Grill");
 var smallPharaoh = new Foodfinder("Middle Eastern", "SW 5th & Stark", "Small Pharaoh");
 var laPinataTakos = new Foodfinder("Mexican", "SW 3rd & Ash", "La Pinata Takos");
 var koiFusion = new Foodfinder("Asian", "2010 W Burnside", "Koi Fusion");
+var someAsianTruck = new Foodfinder("Asian", "2010 W Burnside", "Asian Truck");
 
 var directory = [grilledCheeseGrill, smallPharaoh, laPinataTakos, koiFusion];
 
-var x = "asian"
+var asian=[];
+var userinput = "asian";
 
-var result = directory.find(function(obj){
-  return obj.type === x
+for(i=0; i<directory.length; i++){
+  if(directory[i].type === userinput)
+  asian.push(directory[i]);
+}
 
-});
-console.log(result);
-alert(result.truck);
+console.log(asian);
+
+
+
 
 
 
