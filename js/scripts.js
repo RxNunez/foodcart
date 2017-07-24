b530f4962c56fddedd863a7e45a4655c3b2421d0
kheifer <11204675+kheifer@users.noreply.github.com>
2017-07-24 15:36:50 -0700

    add new foodcarts to our js file

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,37 @@ function Foodfinder(type, location, truck) {
   // this.description = [];
 }
 
-var grilledCheeseGrill = new Foodfinder("American", "SW 10th & Alder", "Grilled Cheese Grill");
 var smallPharaoh = new Foodfinder("Middle Eastern", "SW 5th & Stark", "Small Pharaoh");
 var laPinataTakos = new Foodfinder("Mexican", "SW 3rd & Ash", "La Pinata Takos");
 var koiFusion = new Foodfinder("Asian", "2010 W Burnside", "Koi Fusion");
 var someAsianTruck = new Foodfinder("Asian", "2010 W Burnside", "Asian Truck");
+var altengartz = new Foodfinder("", "", "Altengartz");
+var esanthai = new Foodfinder("asian","", "E-San Thai Cart");
+var traditionalpolish = new Foodfinder("american","","Traditional Polish Cuisine");
+var numberOneBento = new Foodfinder("asian","", "Number 1 Bento");
+var wolfandbear = new Foodfinder("american","", "Wolf and Bear");
+var gyroKing =  new Foodfinder("middleastern","", "Gyro King");
+var zendako = new Foodfinder("asian","", "Zendako");
+var bingMi = new Foodfinder("asian","", "Bing Mi");
+var baghdadiraqi =  new Foodfinder("middleastern","", "Baghdad Iraqi Grill");
+var sawasdeeThai = new Foodfinder("asian","", "Sawasdee Thai");
+var theFryingScotsman = new Foodfinder("","", "The Frying Scotsman");
+var marcoPoloSandwiches = new Foodfinder("","", "Marco Polo's Sandwiches");
+var rollingGourmetFusion = new Foodfinder("asian","", "Rolling Gourmet Fusion");
+var obrososteria =new Foodfinder("american","","O-Bros’ Osteria");
+var samsSahj = new Foodfinder("middleastern","", "Sam's Saj");
+var titosBurritos = new Foodfinder("mexican","", "Tito's Burritos");
+var thaiKeeMao = new Foodfinder("asian","", "Thai Kee Mao");
+var samuraiBento = new Foodfinder("asian","", "Samurai Bento");
+var wholeBowl = new Foodfinder("american","", "The Whole Bowl");
+var gyroPlace = new Foodfinder("middleastern","", "Gyro King");
+var euroDish = new Foodfinder("american","", "Eurodish");
+var nongsKhao =  new Foodfinder("asian","", "Nong's Khao Man Gai");
+var annaThaiBasil = new Foodfinder("asian","", "Anna Thai Basil");
+var alMawj = new Foodfinder("middleastern","", "Al Mawj");
+var grilledCheeseGrill =  new Foodfinder("american","", "The Grilled Cheese Grill")
 
-var directory = [grilledCheeseGrill, smallPharaoh, laPinataTakos, koiFusion, someAsianTruck];
+var directory = [grilledCheeseGrill, smallPharaoh, laPinataTakos, koiFusion, someAsianTruck, altengartz, esanthai, traditionalpolish, numberOneBento, wolfandbear, gyroKing, zendako, bingMi, baghdadiraqi, sawasdeeThai, theFryingScotsman, marcoPoloSandwiches, rollingGourmetFusion, samsSahj, titosBurritos, thaiKeeMao, samuraiBento, wholeBowl, gyroPlace, euroDish, nongsKhao, annaThaiBasil, alMawj, grilledCheeseGrill, potatoesonninth];
 
 var asian=[];
 var userinput = "Asian";
