2e5299fa1ea32fdd64b2eed23155967d8f0e00fe
Bart Webber, Kheifer Fuller and Paul Guevarra <kheifer@>
2017-07-24 10:55:29 -0700

    add constructor and variable to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// Business Logic
+
+function Foodfinder(type, location, truck) {
+  this.type = type;
+  this.location = location;
+  this.truck = truck;
+  this.menuItem = menuItem;
+  this.price = price;
+  this.description = [];
+}
+
+var grilledCheeseGrill = new Foodfinder("American", "SW 10th & Alder", "Grilled Cheese Grill");
+var smallPharaoh = new Foodfinder("Middle Eastern", "SW 5th & Stark", "Small Pharaoh");
+var laPinataTakos = new Foodfinder("Mexican", "SW 3rd & Ash", "La Pinata Takos");
+var koiFusion = new Foodfinder("Asian", "2010 W Burnside", "Koi Fusion");
+
+
+Foodfinder.prototype.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// UI Logic
